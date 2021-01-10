aa917c113dd66119e4e39fbdd2fd19ca1dbe0ecc
OMouad <71100063+OMouad@users.noreply.github.com>
2021-01-10 01:27:07 +0100

    Created the Account class and it's constructor

---
+++ src/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+    private String accountNumber;
+    private String accountholder;
+    private String openDate;
+    private double balance;
+    
+    public Account(String accountNumber,String accountholder,String openDate,double balance){
+        this.accountNumber=accountNumber;
+        this.accountholder=accountholder;
+        this.openDate=openDate;
+        this.balance=balance;
+    }
+    
+    
+}
